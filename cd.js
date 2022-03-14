3b4e55ccc935805ad55e11913684f4f3c831a66b
henrique <henriquecesarl@hotmail.com>
2022-03-13 23:38:28 -0300

    Add GET method

--- cd.js
+++ cd.js
@@ -3,10 +3,16 @@ const fs = require('fs')
 
 const server = express()
 
+server.get('/', (req, res) => {
+  console.log('GET /', req.headers, req.ip)
+
+  res.send('GET root')
+})
+
 server.post('/', (req, res) => {
   console.log('POST /', req.headers, req.ip)
 
-  res.send('Olaa')
+  res.send('POST root')
 })
 
 server.listen(3000, () => {
